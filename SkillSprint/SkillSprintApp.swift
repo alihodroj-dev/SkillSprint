78a4948dda3347457d8811c14f07260698b53005
Ali Hodroj <aloshito2004@gmail.com>
2024-02-15 00:56:36 +0200

    Initial Commit

---
+++ SkillSprint/SkillSprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillSprintApp.swift
+//  SkillSprint
+//
+//  Created by Ali Hodroj on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SkillSprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
